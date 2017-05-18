c0f3f88d32868ef3521a9348b9289b3c156db1f1
JD Bothma <jbothma@gmail.com>
2017-05-18 13:41:07 +0200

    more WIP

--- mpr_analysis/importsep.py
+++ mpr_analysis/importsep.py
@@ -1,9 +1,11 @@
 import logging
 import os
 import sys
-from config import get_session
+from config import DB_URI
 from models import Product, ProductIngredient, Ingredient, ProductSEP
 import xlrd
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 
 name_change = {
@@ -34,20 +36,21 @@ def parse(filename):
     worksheet = workbook.sheet_by_index(0)
 
     product = None
+    skip_ingredients = False
     for idx in range(1, worksheet.nrows):
         nappi_code = worksheet.cell_value(idx, 3)
         regno = worksheet.cell_value(idx, 2).lower()
         pack_size = worksheet.cell_value(idx, 11)
         num_packs = worksheet.cell_value(idx, 12)
         sep = worksheet.cell_value(idx, 16)
-        effective_date = worksheet.cell_value(idx, 18)
+        excel_effective_date = worksheet.cell_value(idx, 18)
         name = worksheet.cell_value(idx, 6).title()
 
         if "medicine" in regno.lower():
             continue
 
         if regno.strip() != "":
-            if product: yield product
+            if product and not skip_ingredients: yield product
 
             generic_value = worksheet.cell_value(idx, 20)
             if "originator" in generic_value.lower():
@@ -61,10 +64,12 @@ def parse(filename):
                 nappi_code = "%s" % int(nappi_code)
             except ValueError:
                 print "Could not process %s (%s) due to lack of nappi code" % (name, regno)
+                skip_ingredients = True
                 continue
 
             if not sep:
                 print "Could not process %s (%s) due to lack of SEP" % (name, nappi_code)
+                skip_ingredients = True
                 continue
             pack_size = pack_size or 1
             num_packs = num_packs or 1
@@ -78,17 +83,20 @@ def parse(filename):
                 "pack_size" : pack_size,
                 "num_packs" : num_packs,
                 "sep" : sep,
-                "effective_date": effective_date,
+                "effective_date": xlrd.xldate.xldate_as_datetime(excel_effective_date, workbook.datemode),
                 "is_generic" : is_generic,
                 "ingredients" : []
             }
+            skip_ingredients = False
 
-        ingredient_name = worksheet.cell_value(idx, 7).title()
-        product["ingredients"].append({
-            "name" : name_change.get(ingredient_name.lower(), ingredient_name),
-            "strength" : worksheet.cell_value(idx, 8),
-            "unit" : worksheet.cell_value(idx, 9).lower(),
-        })
+        if not skip_ingredients:
+            ingredient_name = worksheet.cell_value(idx, 7).title()
+            product["ingredients"].append({
+                "name" : name_change.get(ingredient_name.lower(), ingredient_name),
+                "strength" : worksheet.cell_value(idx, 8),
+                "unit" : worksheet.cell_value(idx, 9).lower(),
+            })
+
 
 
 def main():
@@ -106,9 +114,25 @@ def main():
         except (ValueError, TypeError):
             return None
 
+    engine = create_engine(DB_URI)
+    Session = sessionmaker(bind=engine)
+
     for p in parse(filename):
-        session = get_session()
-        print p
+        session = session = Session()
+        product_count = 0
+        ingredient_count = 0
+        price_count = 0
+        new_product_count = 0
+        new_ingredient_count = 0
+        new_price_count = 0
+        print \
+            "products    %d (%d)\n"\
+            "prices      %d (%d)\n"\
+            "ingredients %d (%d)" % \
+            (product_count, new_product_count,
+             price_count, new_price_count,
+             ingredient_count, new_ingredient_count)
+        print(p)
 
         ingredient_dicts = p['ingredients']
         sep = p['sep']
@@ -118,6 +142,9 @@ def main():
         del p['effective_date']
 
         product, new_product = get_or_create(session, Product, p)
+        product_count += 1
+        if new_product:
+            new_product_count += 1
 
         product_sep, new_product_sep = get_or_create(
             session,
@@ -128,9 +155,12 @@ def main():
                 'effective_date': effective_date,
             }
         )
+        price_count += 1
+        if new_product_sep:
+            new_price_count += 1
 
-        for i in ingredients:
-            strength = i['strenghth']
+        for i in ingredient_dicts:
+            strength = i['strength']
             del i['strength']
             ingredient, new_ingredient = get_or_create(session, Ingredient, i)
 
@@ -143,6 +173,9 @@ def main():
                     'strength': strength,
                 }
             )
+            ingredient_count += 1
+            if new_ingredient:
+                new_ingredient_count += 1
         session.commit()
 
 if __name__ == "__main__":
